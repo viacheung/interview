fce27b349d847166fbb007c351c682174bb28c69
zhangweiyang <505371530@qq.com>
2023-09-28 11:15:13 +0800

    first commit

---
+++ src/main/xiecheng/Main.java
@@ -0,0 +1,14 @@
+package main.xiecheng;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int xp = sc.nextInt();
+        int yp = sc.nextInt();
+        int b=-2*(xp+yp);
+        int c=xp*xp+yp*yp;
+
+    }
+}
